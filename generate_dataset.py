04a6b3a107f8981edfa089c49efc5808edbda4c4
TimmyFan <45388213+TimmyFan@users.noreply.github.com>
2019-01-30 12:12:17 +0800

    Update generate_dataset.py
    Edited eith author Timmy

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,12 +5,11 @@ Created on Tue Jan 29 19:10:05 2019
 @author: Timmy_Fan
 """
 import numpy as np
-#读取数据
+#loading dataset
 def generate_dataset()
     train_data = np.loadtxt(open("train_data.txt","rb"),delimiter=",")  
-    X = train_data[0:50000,0:23]
-    Y = train_data[0:50000,23:24]
+    X = train_data[0:i,0:j] #i is the number of samples in your dataset and j is the number of features in your dataset
+    Y = train_data[0:i,j:j+1] 
     test_data = np.loadtxt(open("test_data.txt","rb"),delimiter=",")  
-    X_test = test_data[0:50000,0:23]
-    Y_test = test_data[0:50000,23:24]
-    return X,Y,X_test,Y_test
+    X_test = test_data[0:i,0:j]
+    return X,Y,X_test
